7672ea699ec2c49954f0535740db005b61cfa57f
thj0309 <thj0309@cj.net>
2022-04-09 11:51:23 +0900

    실전예제 5

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,23 +4,17 @@ package jpabook.jpashop.domain;
 import javax.persistence.*;
 
 @Entity
-public class OrderItem {
+public class OrderItem extends BaseEntity{
     @Id
     @GeneratedValue
     @Column(name="ORDER_ITEM_ID")
     private Long id;
 
-    //@Column(name = "ORDER_ID")
-    //private Long orderId;
-
-    @ManyToOne
-    @JoinColumn(name ="ORDER_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    //@Column(name = "ITEM_ID")
-    //private Long itemId;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="ITEM_ID")
     private Item item;
 
